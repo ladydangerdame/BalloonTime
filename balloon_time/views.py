4ff6c122983e4a59300920dcefb22eb25a142ce3
ladydangerdame <turbofemme@gmail.com>
2018-02-14 13:58:03 -0800

    URL for update fixed thanks to Tai Bayard

--- balloon_time/views.py
+++ balloon_time/views.py
@@ -9,28 +9,24 @@ from django.urls import reverse_lazy
 def party_list(request):
     parties = Party.objects.all()
     form = PartyForm()
-    return render(request, 'party_list.html', {'parties': parties, 'form':form })
+    return render(request,'party_list.html', {'parties':parties, 'form':form})
 
 def party_detail(request, pk):
     party = Party.objects.get(pk=pk)
-    return render(request, 'party_detail.html', {'party': party})
-
-# def post_party(request):
-#     form = PartyForm(request.POST)
-#     if form.is_valid():
-#         party = form.save(commit = False)
-#         party.save()
-#     return HttpResponseRedirect('/')
+    form = PartyForm()
+    return render(request, 'party_detail.html' , {'party' : party, 'form':form})
 
 class PartyCreate(CreateView):
     model = Party
-    fields = ['title', 'location', 'description']
+    fields = ["title","location","description"]
+    success_url = reverse_lazy("party_list")
 
 class PartyDelete(DeleteView):
     model = Party
-    success_url = reverse_lazy('party_list')
+    success_url = reverse_lazy("party_list")
+
 
 class PartyUpdate(UpdateView):
     model = Party
-    fields = ['title', 'location', 'description']
-    success_url = reverse_lazy('party_list')
+    fields = ["title","location","description"]
+    success_url = reverse_lazy("party_list")
