ca85f3a0b02b3a9c915b31c6a240e877182595ae
ladydangerdame <turbofemme@gmail.com>
2018-02-14 14:00:35 -0800

    Updated started code

--- balloon_time/views.py
+++ balloon_time/views.py
@@ -7,15 +7,16 @@ from django.urls import reverse_lazy
 
 
 def party_list(request):
-
+    pass
 
 def party_detail(request, pk):
+    pass
 
-
-def post_party(request):
-
+def post_party(request): #Replace this with CreateView class
+    pass
 
 class PartyDelete(DeleteView):
-
+    pass
 
 class PartyUpdate(UpdateView):
+    pass
